fccd220d33acf86f49b3a95195369f8fe9533718
Thi Nguyen <103225746+maihuynh07@users.noreply.github.com>
2024-07-24 01:34:48 -0400

    - add more variable members as require

--- FinalProject/DiscardCards.h
+++ FinalProject/DiscardCards.h
@@ -6,8 +6,21 @@
 template <class T>
 class DiscardCards {
 public:
-    set<T> cards;  
+    set<T> cards;           // save cards is discarded
+    set<T> cardsPosition;   // save position of cards.
+    int numberOfCards;      // save number of cards is discarded
+    string label;           // save name of class for test
 public:
+    void setCards(set<T> cards){ this->cards = cards;}
+    set<T> getCards(){ return this->cards;}
+    void setCardsPosition(set<T> positions){ this->cardsPosition = positions;}
+    set<T> getCardsPosition(){ return this->cardsPosition;};
+    void setNumberOfCards(int number){
+        this->numberOfCards = number;
+    }
+    int getNumberOfCards(){ return this->numberOfCards;};
+    void setLabel(string label){ this->label = label;};
+    string getLabel(){ return this->label;};
     void insertCard(T c){
         cards.insert(c);
     }
